af13fab0d601c0933d951c8ccd50b645f70a9b0b
ch1huizong <ch1huizong@gmail.com>
2018-10-29 19:34:09 +0800

    add bookmarks

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+urlpatterns = [
+    #path('login/', views.user_login, name='login'),
+    path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+]
