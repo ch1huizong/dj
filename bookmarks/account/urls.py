2d9f11e0794d4e9fb53096b974e6cded95b4315b
ch1huizong <ch1huizong@gmail.com>
2018-10-30 19:47:02 +0800

    password reset

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,5 +8,27 @@ urlpatterns = [
     #path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # change password
+    path('password_change/', 
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    path('password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
+
+    # reset password
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('reset/<uidb64>/token/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
 ]
