d9488776da4ded9ea7db8c60aa0e189fd1d0cc68
ch1huizong <ch1huizong@gmail.com>
2018-11-05 18:16:06 +0800

    add ajax actions to bookmarks

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -6,4 +6,5 @@ app_name = 'images'
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
 ]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .models import Image
 from .forms import ImageCreateForm
@@ -41,3 +43,20 @@ def image_detail(request, id, slug):
         }
     )
 
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ko'})
