fd46c70560cb519c3b55bd499a97a7a46011f3b6
ch1huizong <ch1huizong@gmail.com>
2018-11-06 18:51:27 +0800

    bookmarks增加用户关注功能

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -13,3 +14,27 @@ class Profile(models.Model):
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
 
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set',
+                                on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True) 
+
+    class Meta:
+        ordering = ('-created', )
+
+    def __str__(self):
+        return '{} follows {}'.format(self.user_from, self.user_to)
+
+
+User.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self', 
+        through=Contact, 
+        related_name='followers',
+        symmetrical=False
+    )
+)
