74ed6f657b5db146e9819cb539f04a95d744be3b
ch1huizong <ch1huizong@gmail.com>
2018-11-09 18:10:55 +0800

    bookmarks动态功能完成

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -46,7 +46,8 @@ def dashboard(request):
 
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')\
+                    .prefetch_related('target')[:10]
 
     return render(
         request, 
