553ff46ea39889830475b6bcd07278bfcae0d1c6
ch1huizong <ch1huizong@gmail.com>
2019-07-23 17:54:51 +0800

    add fixtures for testing

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,3 +1,23 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subject, Course, Module
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title', )}
+    inlines = [ModuleInline]
