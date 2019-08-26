20c85f2e6f01b2f2b0b0bbc8c555acaa393162cb
ch1huizong <ch1huizong@gmail.com>
2019-08-26 23:51:48 +0800

    educa configure uwsgi and nginx

--- educa/educa/settings/base.py
+++ educa/educa/settings/base.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = reverse_lazy('students:student_course_list')
 
--- educa/educa/settings/pro.py
+++ educa/educa/settings/pro.py
@@ -6,14 +6,21 @@ ADMINS = (
     ('ch1huizong', 'ch1huizong@gmail.com'),
 )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'educaproject.com',
+    'www.educaproject.com',
+]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'educa',
         'HOST': '192.168.1.10',
-        'USER': 'educa1',
+        'USER': 'educa',
         'PASSWORD': '1234test',
     }
 }
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
