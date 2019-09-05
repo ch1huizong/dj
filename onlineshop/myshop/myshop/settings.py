808f18320e9f7b12d901cbc4010ad2c4919a63af
ch1huizong <ch1huizong@gmail.com>
2019-09-05 19:13:47 +0800

    myshop add shopping cart manager

--- onlineshop/myshop/myshop/settings.py
+++ onlineshop/myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
