1bc4ce59b72ba0ace958c327839c0a321c99697c
ch1huizong <ch1huizong@gmail.com>
2019-09-06 23:36:20 +0800

    myshop add custom actions and custom views

--- onlineshop/myshop/orders/views.py
+++ onlineshop/myshop/orders/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
 
-from .models import OrderItem
+
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 
@@ -31,4 +33,14 @@ def order_create(request):
         request,
         'orders/order/create.html',
         {'form': form, 'cart': cart}
+    )
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request,
+        'admin/orders/order/detail.html',
+        {'order': order}
     )
